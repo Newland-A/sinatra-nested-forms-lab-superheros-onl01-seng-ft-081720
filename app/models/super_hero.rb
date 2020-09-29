223462265567656c4199633a9f5bb14adaa40839
Newland-A <ajn252@gmail.com>
2020-09-29 09:41:45 -0400

    Done.

---
+++ app/models/super_hero.rb
@@ -0,0 +1,22 @@
+class SuperHero
+
+  attr_reader :name, :power, :bio
+
+  SUPERHERO = []
+
+  def initialize(params)
+    @name = params[:name]
+    @power = params[:power]
+    @bio = params[:bio]
+    SUPERHERO << self
+  end
+
+  # def save
+  #   self.class.all << self
+  # end
+
+  def self.all
+    SUPERHERO
+  end
+
+end
